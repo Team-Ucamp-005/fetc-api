2e66b7625bfcab5a5f3de64e4e82cd998c86c437
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-04-30 12:59:15 -0500

    agregar fetch marvel api

--- app.js
+++ app.js
@@ -2,6 +2,34 @@ console.log('Hola mundo')
 
 
 
-const getCharacters = () => {
-  const url = `https://gateway.marvel.com:443/v1/public/characters`
+const getCharacters = async () => {
+
+  // hash key: md5(ts + private key + public key)
+
+  const url = `https://gateway.marvel.com:443/v1/public/characters?ts=1651339658&apikey=<public key>&hash=<hash key>&limit=50`
+
+  const getData = await fetch(url)
+
+  const data = await getData.json()
+
+  console.log('data', data)
+
+}
+
+const getCharacterName = async () => {
+
+  const input = ''
+
+  const url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${input}ts=1651339658&apikey=<public key>&hash=<hash key>&limit=50`
+
+  const getData = await fetch(url)
+
+  const data = await getData.json()
+
+  console.log('data', data)
+}
+
+const validateInput = () => {
+  getCharacters()
+  getCharacterName()
 }
