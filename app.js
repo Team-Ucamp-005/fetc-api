c6bd9ddf782051cb40689db0640b35a7c50462bc
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-04-30 10:56:12 -0500

    ejercicio clima

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-console.log('Hola mundo')
+console.log('hola')
 
+const getWeather = async () => {
 
+  // let data = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Ottawa&appid=616629f9acdc3b22b8b09553e632e5da")
+  // const res = await data.json();
+  // console.log(res);
 
-const getCharacters = () => {
-  const url = `https://gateway.marvel.com:443/v1/public/characters`
+  const ciudad = document.getElementById('ciudad').value
+
+  let container = document.getElementById('contain-info')
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad === '' ? 'Mexico' : ciudad}&appid=616629f9acdc3b22b8b09553e632e5da`)
+    .then(response => response.json())
+    .then(res => {
+      container.innerHTML = `
+        <p>
+          ${res.name}
+        </p>
+        <p>
+          ${res.main.temp}
+        </p>
+      `
+    })
 }
+
+getWeather()
