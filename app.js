e5fe5e183077c7cd4fc02796a3e822a8b640f4d2
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-04-30 13:58:12 -0500

    agrega buscador de heroes

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ console.log('Hola mundo')
 
 
 const getCharacters = async () => {
-
-  // hash key: md5(ts + private key + public key)
-
-  const url = `https://gateway.marvel.com:443/v1/public/characters?ts=1651339658&apikey=<public key>&hash=<hash key>&limit=50`
+  const url = `https://gateway.marvel.com:443/v1/public/characters?ts=1651339658&apikey=35755cd339fd7c676339edf8d57fcc37&hash=324da40eb91c741cbd78f922f02b65f5&limit=50`
 
   const getData = await fetch(url)
 
@@ -18,18 +15,35 @@ const getCharacters = async () => {
 
 const getCharacterName = async () => {
 
-  const input = ''
+  const input = document.getElementById('personaje').value
 
-  const url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${input}ts=1651339658&apikey=<public key>&hash=<hash key>&limit=50`
+  const url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${input}&ts=1651339658&apikey=35755cd339fd7c676339edf8d57fcc37&hash=324da40eb91c741cbd78f922f02b65f5&limit=50`
 
   const getData = await fetch(url)
 
   const data = await getData.json()
 
-  console.log('data', data)
+  console.log(data)
+
+  renderData(data.data.results)
+
+}
+
+
+const renderData = (results) => {
+  const container = document.getElementById('container-data')
+  container.innerHTML = ''
+  results.forEach(item => {
+    container.innerHTML += `
+      <div>
+        <img src="${item.thumbnail.path + '.' + item.thumbnail.extension}" />
+        <p>${item.name}</p>
+        <p></p>
+      </div>
+    `
+  })
 }
 
 const validateInput = () => {
   getCharacters()
-  getCharacterName()
 }
